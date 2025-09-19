d13a54b0d552d643fa16a40b54f82d05793b6200
AnnaKarpovi <anna.karpoffa@gmail.com>
2025-09-19 16:08:09 +0300

    previous commits

--- minitorch/module.py
+++ minitorch/module.py
@@ -31,11 +31,15 @@ class Module:
 
     def train(self) -> None:
         "Set the mode of this module and all descendent modules to `train`."
-        raise NotImplementedError("Need to include this file from past assignment.")
+        self.training = True
+        for m in self.modules():
+            m.train()
 
     def eval(self) -> None:
         "Set the mode of this module and all descendent modules to `eval`."
-        raise NotImplementedError("Need to include this file from past assignment.")
+        self.training = False
+        for m in self.modules():
+            m.eval()
 
     def named_parameters(self) -> Sequence[Tuple[str, Parameter]]:
         """
@@ -45,11 +49,21 @@ class Module:
         Returns:
             The name and `Parameter` of each ancestor parameter.
         """
-        raise NotImplementedError("Need to include this file from past assignment.")
+        params_list = []
+        for name, param in self._parameters.items():
+            params_list.append((name, param))
+
+        for module_name, module in self._modules.items():
+            child_params = module.named_parameters()
+            for child_name, child_param in child_params:
+                final_name = f"{module_name}.{child_name}"
+                params_list.append((final_name, child_param))
+        return params_list
+
 
     def parameters(self) -> Sequence[Parameter]:
         "Enumerate over all the parameters of this module and its descendents."
-        raise NotImplementedError("Need to include this file from past assignment.")
+        return [param for name, param in self.named_parameters()]
 
     def add_parameter(self, k: str, v: Any) -> Parameter:
         """
